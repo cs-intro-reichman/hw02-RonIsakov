c9de67252acc6387e5035defd331b9e0c2a1c196
RonIsakov <154628459+RonIsakov@users.noreply.github.com>
2023-12-27 22:41:33 +0200

    Update DamkaBoard.java

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -3,6 +3,19 @@
  */
 public class DamkaBoard {
 	public static void main(String[] args) {
-		//// Put your code here
-	}
+		//user inserting int num as dimensions of the board
+		 int num = Integer.parseInt(args[0]);
+
+        // Printing the Damka board with num*num dimensions
+        for (int i = 0; i < num; i++) {
+            for (int j = 0; j < num; j++) {
+                if ((i + j) % 2 == 0) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
 }
