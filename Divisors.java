004cd4d6223701e144dafbf761fdc1e1f3035974
RonIsakov <154628459+RonIsakov@users.noreply.github.com>
2023-12-27 22:42:10 +0200

    Update Divisors.java

--- Divisors.java
+++ Divisors.java
@@ -2,7 +2,17 @@
  *  Gets a command-line argument (int), and prints all the divisors of the given number.
  */
 public class Divisors {
-	public static void main (String[] args) {
-		//// Put your code here
+public static void main (String[] args) {
+	// the user inserts the x
+	int x = Integer.parseInt(args[0]);
+	
+// a loop that test witch of the numbers are a devisor of x
+for(int i = 1; i<=x ; i++){
+	if(x%i == 0){
+	System.out.println(i);
+	}
+}
+
+
 	}
 }
