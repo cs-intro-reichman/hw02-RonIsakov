7ea6653b36fb5f6aa516f6c6176225a187491c84
RonIsakov <154628459+RonIsakov@users.noreply.github.com>
2023-12-27 22:46:32 +0200

    Update OneOfEach.java

--- OneOfEach.java
+++ OneOfEach.java
@@ -5,6 +5,34 @@
  */
 public class OneOfEach {
 	public static void main (String[] args) {
-		//// Put your code here
+		
+	// defining int random witch generates a number between 10 and 0 include
+	int random = (int)(Math.random() * 11);
+	int counter = 0;
+	boolean b = false;
+	boolean g = false;
+	
+	while(!b || !g){
+		if(random <= 5){
+			b = true;
+			if(counter == 0)
+			System.out.print("b" );
+		else 
+			System.out.print(" + b");
+		}
+		else{
+			g = true;
+			if(counter == 0)
+			System.out.print("g" );
+		else 
+			System.out.print(" + g");
+		}
+		random = (int)(Math.random() * 11);
+		counter ++;
+	}
+	System.out.println();
+	System.out.println("You made it... and you now have " + counter + " children.");
+
+			
 	}
 }
