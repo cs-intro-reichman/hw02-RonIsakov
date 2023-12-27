3e2e6df49a3e836ddae522655757514c06fa41ba
RonIsakov <154628459+RonIsakov@users.noreply.github.com>
2023-12-27 22:44:43 +0200

    Update InOrder.java

--- InOrder.java
+++ InOrder.java
@@ -4,6 +4,17 @@
  */
 public class InOrder {
 	public static void main (String[] args) {
-		//// Write your code here
+		// defining 2 ints and setting num2 as a random num
+		//between 0 inc and 10
+	int num1 = 0;
+	int num2 = (int)(Math.random() * 10);
+	
+	// defining a loop while the number that was generated is bigger or even 
+	// to the number befor it prints it 
+	while (num1 <= num2){
+		System.out.println(num2);
+		num1 = num2;
+		num2 = (int)(Math.random() * 10);
+	}
 	}
 }
