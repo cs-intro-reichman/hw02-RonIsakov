38c934edb2be6215b999b3d675081d2fbd6ceaaa
RonIsakov <154628459+RonIsakov@users.noreply.github.com>
2023-12-28 22:51:45 +0200

    Update OneOfEachStats1.java

--- OneOfEachStats1.java
+++ OneOfEachStats1.java
@@ -14,7 +14,10 @@ public class OneOfEachStats1 {
 	int childrenmax = 0;
 	boolean b = false;
 	boolean g = false;
+	double avrage = 0;
 	 int T = Integer.parseInt(args[0]);
+	 
+	 
 	 // main loop runs the amount of times the user sets
 	 for(int i = 0;i < T; i++){
 		 //while loop runs the test to see how many children are in the family to have a boy and a gril
@@ -38,10 +41,14 @@ public class OneOfEachStats1 {
 	 random = (int)(Math.random() * 11);
 	 b = false;
      g = false;
+	 avrage = avrage + counter;
      counter = 0;
+	 
 		 
 	} 
 	// printing the results of how many families are in each category
+	avrage = avrage/T;
+	System.out.println("Avrege: " + avrage + " children to get at least one of each gender.");
 	System.out.println("Number of families with 2 children: " + children2);
 	System.out.println("Number of families with 3 children: " + children3);
 	System.out.println("Number of families with 4 or more children: " + childrenmax);
