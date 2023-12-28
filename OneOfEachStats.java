5283018db070081e3f8f60238577f1f691e25bbb
RonIsakov <154628459+RonIsakov@users.noreply.github.com>
2023-12-28 23:12:20 +0200

    Update OneOfEachStats.java

--- OneOfEachStats.java
+++ OneOfEachStats.java
@@ -14,16 +14,64 @@ public class OneOfEachStats {
 		int seed = Integer.parseInt(args[1]);
 		// Initailizes a random numbers generator with the given seed value
         Random generator = new Random(seed);  
-		
-		//// In the previous version of this program, you used a statement like:
-		//// double rnd = Math.random();
-		//// Where "rnd" is the variable that stores the generated random value.
-		//// In this version of the program, replace this statement with:
-		//// double rnd = generator.nextDouble();
-		//// This statement will generate a random value in the range [0,1),
-		//// just like you had in the previous version, except that the 
-		//// randomization will be based on the given seed.
-		//// This is the only change that you have to do in the program.
-		    
+
+	double rnd = generator.nextDouble();
+	int counter = 0;
+	int children2 = 0;
+	int children3 = 0;
+	int childrenmax = 0;
+	boolean b = false;
+	boolean g = false;
+	double avrage = 0;
+	 
+	 // main loop runs the amount of times the user sets
+	 for(int i = 0;i < T; i++){
+		 //while loop runs the test to see how many children are in the family to have a boy and a gril
+		while(!b || !g){
+		if(rnd <= 0.5){
+			b = true;
+		}
+		else{
+			g = true;
+		}
+		counter ++;
+		rnd = generator.nextDouble();
+	 }
+	 
+	 //puts the amount of kids in a family in their catagory
+	 if(counter == 2) children2 ++;
+	 if(counter == 3) children3 ++;
+	 if(counter >= 4) childrenmax ++;
+	 
+	 // resets the varubals so that the test can run again 
+	 rnd = generator.nextDouble();
+	 b = false;
+     g = false;
+	 avrage = avrage + counter;
+     counter = 0;
+	 
+		 
+	} 
+	// printing the results of how many families are in each category
+	avrage = avrage/T;
+	System.out.println("Avrege: " + avrage + " children to get at least one of each gender.");
+	System.out.println("Number of families with 2 children: " + children2);
+	System.out.println("Number of families with 3 children: " + children3);
+	System.out.println("Number of families with 4 or more children: " + childrenmax);
+	
+	// determens and prints the most fruquent catagory of family
+	if((children2 > children3) && (children2 > childrenmax)){
+		System.out.println("The most common number of children is 2.");
 	}
+		else{
+			if(children3 > childrenmax){
+					System.out.println("The most common number of children is 3.");
+			}
+			else{
+					System.out.println("The most common number of children is 4 or more children.");
+			}
+		}
 }
+}
+
+
