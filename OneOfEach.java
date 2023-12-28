a6eeae63f8d410e9aa8c4a0a4f84840c51fb9871
RonIsakov <154628459+RonIsakov@users.noreply.github.com>
2023-12-28 08:49:45 +0200

    Update OneOfEach.java

--- OneOfEach.java
+++ OneOfEach.java
@@ -8,28 +8,33 @@ public class OneOfEach {
 		
 	// defining int random witch generates a number between 10 and 0 include
 	int random = (int)(Math.random() * 11);
+	
+	//defining varubals
 	int counter = 0;
 	boolean b = false;
 	boolean g = false;
 	
+	//loop that tests how many children there are until there is a boy and a gril
 	while(!b || !g){
 		if(random <= 5){
 			b = true;
 			if(counter == 0)
-			System.out.print("b" );
+			System.out.print("b");
 		else 
-			System.out.print(" + b");
+			System.out.print(" b");
 		}
 		else{
 			g = true;
 			if(counter == 0)
-			System.out.print("g" );
+			System.out.print("g");
 		else 
-			System.out.print(" + g");
+			System.out.print(" g");
 		}
 		random = (int)(Math.random() * 11);
 		counter ++;
 	}
+	
+	//printing the amount of children
 	System.out.println();
 	System.out.println("You made it... and you now have " + counter + " children.");
 
